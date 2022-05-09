0f260845d1e0dfd319508db7af81b89c2a2f18bd
ViniciusSouza <vinicius.souza@napista.com.br>
2022-05-08 21:17:36 -0300

    Criação e aplicação de alguns components

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
-
+import Form from './components/Form';
+import List from './components/List';
 function App() {
   return (
     <div className="App">
-      Hello Svck
+      <Form />
+      <List />
     </div>
   );
 }
---
+++ src/components/List/index.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+function List(){
+    return(
+        <aside>
+            <h2> Studies of day</h2>
+            <ul>
+                <li>
+                    <h3>
+                        Unity
+                    </h3>
+                    <span>
+                        02:00:00
+                    </span>
+                </li>
+                <li>
+                    <h3>
+                        GMS 2
+                    </h3>
+                    <span>
+                        01:30:00
+                    </span>
+                </li>
+            </ul>
+        </aside>
+    )
+}
+
+export default List;
